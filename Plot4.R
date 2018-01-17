711924e12950d2d313cdc2d6af0c08326e17ad4e
Deepak Valagam <vrdeepakprakash@gmail.com>
2018-01-17 16:54:13 +0530

    Changes

--- Plot4.R
+++ Plot4.R
@@ -5,10 +5,10 @@ library(ggplot2)
 #Get totals
 code <- grepl("Coal",SCC$EI.Sector)
 code <- as.character(SCC[code,]$SCC)
-t <- summary[summary$SCC%in%codes,]
+t <- summary[summary$SCC%in%code,]
 
 #Plotting
 png(filename = "Plot4.png")
-p <-qplot(year,Emissions,data = t,xlab = "Year",ylab = "Emissions (Tons)")+geom_smooth(method = "lm") + coord_cartesian(ylim = c(0,300))
+p <-qplot(year,Emissions,data = t,xlab = "Year",ylab = "Emissions (Tons)")+geom_smooth(method = "lm") + coord_cartesian(ylim = c(0,100))
 print(p)
 dev.off()
